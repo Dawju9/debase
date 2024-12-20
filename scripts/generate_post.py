7eb06b025ffa6e08557d5c14b9d129ecd5ce1812
A ya Whitekali <dawju9@gmail.com>
2024-12-21 00:06:15 +0100

    `Removed unused import statement`

--- scripts/generate_post.py
+++ scripts/generate_post.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # Wczytanie modelu i tokenizer
 # Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
+#from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
